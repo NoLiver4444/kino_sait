a4dbd9f91903ec1ed13bf963af511d1c1d2fa4d5
qwer <dprslt@inbox.ru>
2024-08-19 01:47:05 +0300

    qwer

--- my-app/src/choose/choose.js
+++ my-app/src/choose/choose.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./choose.css"
 
+function Choose_Text(props) {
+    //Обращение к бд для инфы о выборе
+    if (props.btn_id === "kino") {
+        return (
+            <h1>a</h1>
+        );
+    } else if (props.btn_id === "kniga") {
+        return (
+            <h1>b</h1>
+        );
+    } else if (props.btn_id === "serial") {
+        return (
+            <h1>c</h1>
+        );
+    } else if (props.btn_id === "anime") {
+        return (
+            <h1>d</h1>
+        );
+    }
+}
+
 class Choose extends React.Component {
     constructor(props){
         super(props);
@@ -15,64 +36,48 @@ class Choose extends React.Component {
         });
     };
 
-    choose_text = (btn_id) => {
-        //Обращение к бд для инфы о выборе
-        if (btn_id === "kino") {
-            return (
-                <h1>a</h1>
-            );
-        } else if (btn_id === "kniga") {
-            return (
-                <h1>b</h1>
-            );
-        } else if (btn_id === "serial") {
-            console.log(1);
-            return (
-                <h1>c</h1>
-            );
-        } else if (btn_id === "anime") {
-            return (
-                <h1>d</h1>
-            );
-        }
+    buttons_render = (btn_id) => {
+        return (
+            <div class="name">
+                <button
+                    id="kino"
+                    onClick={this.cheng_click_id}
+                    type="button"
+                    className={"kino " + (btn_id === "kino" ? "click" : "")}
+                >
+                    ФИЛЬМЫ
+                </button>
+                <button 
+                    id="kniga"
+                    onClick={this.cheng_click_id}
+                    className={"kniga " + (btn_id === "kniga" ? "click" : "")} 
+                >
+                    КНИГИ
+                </button>
+                <button  
+                    id="serial"
+                    onClick={this.cheng_click_id}
+                    className={"serial " + (btn_id === "serial" ? "click" : "")}
+                >
+                    СЕРИАЛЫ
+                </button>
+                <button  
+                    id="anime"
+                    onClick={this.cheng_click_id}
+                    className={"anime " + (btn_id === "anime" ? "click" : "")}
+                >
+                    АНИМЕ
+                </button>
+            </div>
+        )
     }
 
     render() {
         return (
             <div class="choose">
-                <div class="name">
-                    <button
-                        id="kino"
-                        onClick={this.cheng_click_id}
-                        type="button"
-                        className={"kino " + (this.state.btn_id === "kino" ? "click" : "")}
-                    >
-                        ФИЛЬМЫ
-                    </button>
-                    <button 
-                        id="kniga"
-                        onClick={this.cheng_click_id}
-                        className={"kniga " + (this.state.btn_id === "kniga" ? "click" : "")} 
-                    >
-                        КНИГИ
-                    </button>
-                    <button  
-                        id="serial"
-                        onClick={this.cheng_click_id}
-                        className={"serial " + (this.state.btn_id === "serial" ? "click" : "")}
-                    >
-                        СЕРИАЛЫ
-                    </button>
-                    <button  
-                        id="anime"
-                        onClick={this.cheng_click_id}
-                        className={"anime " + (this.state.btn_id === "anime" ? "click" : "")}
-                    >
-                        АНИМЕ
-                    </button>
-                </div>
+                {this.buttons_render(this.state.btn_id)}
                 <div class="choose_info">
-                    {this.choose_text(this.state.btn_id)}
+                    <Choose_Text btn_id={this.state.btn_id}/>
                 </div>
             </div>
         )
